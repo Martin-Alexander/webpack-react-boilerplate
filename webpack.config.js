0fe0dab4ad242f1c2737c818e9754402a8dee557
Martin-Alexander <martingianna@gmail.com>
2018-05-27 20:17:43 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'templates/index.html',
+      inject: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+      chunkFilename: 'bundle.css'
+    })    
+  ],
+  devtool: 'sourcemap',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  }
+};
